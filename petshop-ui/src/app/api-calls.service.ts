600b81b4c2912d1dbf1ef76775bf1f823a354794
C9HS-TISTA <51930884+C9HS-TISTA@users.noreply.github.com>
2020-08-28 18:04:43 -0400

    GET call for BC

--- petshop-ui/src/app/api-calls.service.ts
+++ petshop-ui/src/app/api-calls.service.ts
@@ -12,29 +12,56 @@ export class ApiCallsService {
   constructor(private http: HttpClient) { }
 
   getRequest(businessName: string): Observable<any> {
-
+  //  const url = 'http://localhost:8080/api/v1/business/name/'+businessName;
+  //  const url = 'https://fveiyib1lg.execute-api.us-east-1.amazonaws.com/dev/myrestgw?transactionId=1&type=Pur&amount=600';
+  //  const url = 'https://rmsxqazen1.execute-api.us-east-1.amazonaws.com/tista-mock-api-dev/api?queryStringParameters'+businessName;
+  //  const url = 'https://rmsxqazen1.execute-api.us-east-1.amazonaws.com/tista-mock-api-dev/businessname';
+  const url = 'http://54.157.159.142:4000/v1/businessstatus';
+//  var urlencoded = new URLSearchParams();
+// urlencoded.append("queryStringParameters", "testsandeep");
     const options = {
+    
       'headers': {
         Accept: 'application/json',
-        'x-api-key': 'Api-Key 36QwZFehatFQwzcVYMzxfkgGxT9m5bjP',
-        // 'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/x-www-form-urlencoded',
-        "Access-Control-Allow-Origin" : '*',
-        "Access-Control-Allow-Methods" : 'GET,POST,PATCH,DELETE,PUT,OPTIONS',
-        "Access-Control-Allow-Headers" :'Origin, Content-Type, X-Auth-Token, content-type'
-        // businessName: businessName
-      },
-      data: {
-        businessName: businessName
-      },
-      mode: 'no-cors'
+// // BLOCKCHAIN
+'x-api-key': 'Api-Key 36QwZFehatFQwzcVYMzxfkgGxT9m5bjP'
+//         // 'ContentType': 'application/x-www-form-urlencoded'
 
+// //VEMS API 
+//          'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJteXVzZXJuYW1lIiwiZXhwIjoxNTk5MTY3Mjc5fQ.7_wRjuqtUr0YOwIWtZKXisLLD3t78pC4s4WckgiK2D-cUgNEeqgQW1nuy10pGjAh7T8fXOa1aWnD4bo2NN4a4Q'
+
+      //   'Access-Control-Allow-Origin': '*'
+//         // 'Content-Type': 'application/json',
+// "sec-fetch-dest":"empty",
+// "sec-fetch-mode":"cors",
+// "sec-fetch-site":"cross-site",
+      
+//        //  "Access-Control-Allow-Origin" : '*'
+//         //  "Access-Control-Allow-Methods" : 'GET,POST,PATCH,DELETE,PUT,OPTIONS',
+//         //  "Access-Control-Allow-Headers" :'Origin, Content-Type, X-Auth-Token, content-type'
+//       //   // businessName: businessName
+      }   //  ,
+// BLOCKCHAIN
+
+  //  data: {
+  //     "businessName": businessName
+  //    }
+    
     };
+    const form = 'businessName=' + businessName;
+    
+// BLOCKCHAIN
     // return this.http.get(
-    //   'http://54.157.159.142:4000/v1/businessstatus',options
+    //   'https://rmsxqazen1.execute-api.us-east-1.amazonaws.com/tista-mock-api-dev/businessname',options
+    //  // 'http://54.157.159.142:4000/v1/businessstatus',options
     // );
 
-    return this.http.get<any>('http://54.157.159.142:4000/v1/businessstatus', options);
+//VEMS API 
+return this.http.post(url, form, options);
+//   return this.http.get<any>(url,options);
+//WEATHER FORECAST
+  //  return this.http.get('https://api.aerisapi.com/forecasts/minneapolis,mn?&format=json&filter=day&limit=7&client_id=AcxJ7pqDEeRA8kcDUOTPS&client_secret=srQU1W4bKuWF1E660KRBsAgJMeOFmTY1Vhl3csAM', options);
  }
 
  sendRequest(form: any) {
